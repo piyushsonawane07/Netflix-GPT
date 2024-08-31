e6db96d49dadbf4e8a292f1e1184aae6c8c9be4f
piyushsonawane07 <piyushsonawane140@gmail.com>
2024-08-31 10:50:35 +0530

    env add

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,4 @@
+
 export const LOGO_URL =
   "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
 
@@ -5,11 +6,10 @@ export const TMDB_API_OPTIONS = {
   method: "GET",
   headers: {
     accept: "application/json",
-    Authorization:
-      "",
+    Authorization: process.env.TMDB_AUTH,
   },
 };
 
 export const POSTER_CDN_URL = "https://image.tmdb.org/t/p/w200"
 
-export const OPENAI_API_KEY = ""
+export const OPENAI_API_KEY = process.env.OPEN_AI_KEY
